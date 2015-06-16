94a0af8a52d7657ac580106cf53ad4853809010f
Andoni Aguirrezabal <aaguirrezabal@live.com>
2015-06-16 12:07:56 -0400

    Spiffed up OPEAssist
    Signed-off-by: Andoni Aguirrezabal <aaguirrezabal@live.com>

--- OPEAssist.py
+++ OPEAssist.py
@@ -1,3 +1,27 @@
+# Copyright (c) 2014, Andoni Aguirrezabal
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import subprocess
 
 objCount = 0
@@ -6,9 +30,10 @@ selectedObject = 0
 tablePos = []
 tableSize= []
 
+
 def runOPE(wait=True):
     ope_process = subprocess.Popen("./ope-init.sh", shell=True,
-                                   cwd="./OPE-Release/")
+                                                            cwd="./OPE-Release/")
     if wait:
         ope_process.wait()
 
@@ -53,4 +78,4 @@ def loadOPEResults():
 
 def showOPEResults():
     subprocess.Popen("ristretto output.png", shell=True,
-                     cwd="./OPE-Release/")
+                                   cwd="./OPE-Release/")
